f6438b731969f028cb08ec0dd72dda2c4dffc530
Pomolokoki <113830307+Pomolokoki@users.noreply.github.com>
2024-04-04 08:16:37 +0200

    Initial commit

---
+++ ChercheMots/Ihm/AnagrammesWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChercheMots.Ihm
+{
+    /// <summary>
+    /// Logique d'interaction pour AnagrammesWindow.xaml
+    /// </summary>
+    public partial class AnagrammesWindow : Window
+    {
+        private Metier.Dictionnaire dico;
+        public AnagrammesWindow(Metier.Dictionnaire dico)
+        {
+            InitializeComponent();
+            this.dico = dico;
+        }
+
+        private void Chercher(object sender, RoutedEventArgs e)
+        {
+            var list = dico.Anagrammes(mot.Text);
+            mots.Items.Clear();
+            foreach (var s in list)
+                mots.Items.Add(s);
+        }
+    }
+}
---
+++ ChercheMots/Ihm/DefinitionsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChercheMots.Ihm
+{
+    /// <summary>
+    /// Logique d'interaction pour DefinitionsWindow.xaml
+    /// </summary>
+    public partial class DefinitionsWindow : Window
+    {
+        private Metier.Dictionnaire dico;
+        public DefinitionsWindow(Metier.Dictionnaire dico)
+        {
+            InitializeComponent();
+            this.dico = dico;
+        }
+
+        private void Chercher(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                def.Text = dico.Definition(mot.Text);
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+    }
+}

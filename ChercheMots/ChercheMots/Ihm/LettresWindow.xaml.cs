bb3da472e2852582dadf52aec2a67f728d36bba2
Pomolokoki <113830307+Pomolokoki@users.noreply.github.com>
2024-04-08 13:46:09 +0200

    allfix

---
+++ ChercheMots/ChercheMots/Ihm/LettresWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChercheMots.Ihm
+{
+    /// <summary>
+    /// Logique d'interaction pour LettresWindow.xaml
+    /// </summary>
+    /// <author>
+    /// Thibault Callerand
+    /// </author>
+    public partial class LettresWindow : Window
+    {
+        private Metier.Dictionnaire dico;
+        public LettresWindow(Metier.Dictionnaire dico)
+        {
+            InitializeComponent();
+            this.dico = dico;
+        }
+
+        private void Chercher(object sender, RoutedEventArgs e)
+        {
+            var list = dico.MotsAvecLettres(mot.Text, chekBool.IsChecked.GetValueOrDefault());
+            mots.Items.Clear();
+            foreach (var s in list)
+                mots.Items.Add(s);
+        }
+    }
+}
---
+++ ChercheMots/ChercheMots/Ihm/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChercheMots
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Metier.Dictionnaire dico ;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                // récupère le chemin du dictionnaire, relatif à l'exécutable
+                string fichier = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+@"\Ressources\dico.fr";
+                // charge le dictionnaire
+                Metier.IChargeDico chargeur = new Stockage.ChargeFichier(fichier);
+                dico = chargeur.Charger();
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        private void Anagrammes(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre anagrammes
+            Ihm.AnagrammesWindow win = new Ihm.AnagrammesWindow(dico);
+            win.Show();
+        }
+
+        private void Definitions(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre définitions
+            Ihm.DefinitionsWindow win = new Ihm.DefinitionsWindow(dico);
+            win.Show();
+        }
+
+        /// <author>Hubert Tom</author>
+        private void Prefixes(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre prefixes
+            Ihm.PrefixesWindow win = new Ihm.PrefixesWindow(dico);
+            win.Show();
+        }
+
+        /// <author>Hubert Tom</author>
+        private void Palindromes(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre Palindromes
+            Ihm.PalindromesWindow win = new Ihm.PalindromesWindow(dico);
+            win.Show();
+        }
+
+        /// <author>Callerand Thibault</author>
+        private void RechercheLettres(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre définitions
+            Ihm.LettresWindow win = new Ihm.LettresWindow(dico);
+
+            win.Show();
+        }
+
+        /// <author>Hubert Tom</author>
+        private void MotsN(object sender, RoutedEventArgs e)
+        {
+            // ouvre la fenêtre prefixes
+            Ihm.MotsNWindow win = new Ihm.MotsNWindow(dico);
+            win.Show();
+        }
+    }
+}
